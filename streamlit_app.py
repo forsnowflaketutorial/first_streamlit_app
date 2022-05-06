509d58cf5016817c57b691a07aaad2cd922bec00
forsnowflaketutorial <105026680+forsnowflaketutorial@users.noreply.github.com>
2022-05-06 13:01:08 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 
 streamlit.title("My parentsnew healthy Diner")
+streamlit.text('Omega 3 & Blueberry outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
